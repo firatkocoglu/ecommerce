5f7cb4b08945e87c89f8dfde64e1e9ed7bab1914
firatkocoglu <fratkocogl@gmail.com>
2025-08-20 20:29:09 +0300

    Add index controller for categories

--- app/Http/Controllers/API/V1/Categories/CategoryAPIController.php
+++ app/Http/Controllers/API/V1/Categories/CategoryAPIController.php
@@ -4,8 +4,28 @@ namespace App\Http\Controllers\API\V1\Categories;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Services\Categories\CategoryService;
+use App\Http\Resources\API\V1\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
 
 class CategoryAPIController extends Controller
 {
-    //
+    public function __construct(private readonly CategoryService $service){
+
+    }
+
+    /**
+     * GET /api/v1/categories
+     * Paginated list
+     */
+
+    public function index(): JsonResponse
+    {   
+        $perPage = (int) request('per_page', 20);
+
+        $paginator = $this->service->listPaginated($perPage);
+
+        return CategoryResource::collection($paginator)->response();
+    } 
 }
