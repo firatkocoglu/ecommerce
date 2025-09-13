0bcd0c10134a0d75c79173853186bcd9066ded35
firatkocoglu <fratkocogl@gmail.com>
2025-09-13 19:13:47 +0300

    fix(migrations): correct table name in down method for category_product pivot table

--- database/migrations/2025_08_20_151203_create_category_product_pivot_table.php
+++ database/migrations/2025_08_20_151203_create_category_product_pivot_table.php
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('category_product_pivot');
+        Schema::dropIfExists('category_product');
     }
 };
