78e00b4a189aa71803432de8a6cf15cc68618374
firatkocoglu <fratkocogl@gmail.com>
2025-10-19 22:27:41 +0300

    feat(database): add source_cart_id field to orders, rename gross_unit_price to unit_gross_price in order_items, and add name field to cart_items

---
+++ database/migrations/2025_10_19_124615_add_source_cart_id_field_into_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+           $table->bigInteger('source_cart_id')->unsigned()->nullable();
+        });
+
+        DB::statement(
+            'CREATE UNIQUE INDEX IF NOT EXISTS unique_source_cart_id_index
+                    ON orders (source_cart_id)
+                    WHERE status=\'pending\''
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('source_cart_id');
+        });
+
+        DB::statement(
+            'DROP INDEX IF EXISTS unique_source_cart_id_index'
+        );
+    }
+};
